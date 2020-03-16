8575f2462b5d6bf51ba86de845e6457bce1f2fec
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-16 23:15:47 +0000

    urlDatabase object added to request list

--- express_server.js
+++ express_server.js
@@ -4,12 +4,17 @@ const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.goog.le.com"
+  "9sm5xK": "http://www.google.com"
 };
 
-// Same as requestHandler in app.js, if request is received with not path after URL, Hello will be the response back to the listener
+// Same as requestHandler in app.js, if request is received with no path after URL, Hello will be the response back to the listener
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// If request is received with /urls.json path, the urlDatabase object will be the response back to the listener
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 // Using EXPRESS to listen port request and Printing message to to notify of listener
