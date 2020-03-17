d18596655bba81e9a2bc54547cc569e68912e63b
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-17 05:00:25 +0000

    added generate random string function

--- express_server.js
+++ express_server.js
@@ -1,24 +1,20 @@
+function generateRandomString() {
+
+}
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// converts body from buffer to string that can be read
+// It then adds the data to the request object under the key body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Database of all the shortened URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-// Same as requestHandler in app.js, if request is received with no path after URL, Hello will be the response back to the listener
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-// new route handler that passes the urls.index file the URL data via res.render when /url is requested
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
 
 // Tells the express application to use EJS as template engine
 app.set("view engine", "ejs");
@@ -28,6 +24,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// HTML markup used with output string, can cause issues
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body</html>\n");
+});
+
+// Same as requestHandler in app.js, if request is received with no path after URL, Hello will be the response back to the listener
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// Add a route handler to retrieve a new URL and use the formatting from urls_new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // Passing the shortURL in the browser (providing the longURL is defined), will return the request back using the template in the urls_show file.
 // shortURL is declared when passing the shortURL in the browser, so the longURL is be defined using shortURL as the object key
 app.get("/urls/:shortURL", (req, res) => {
@@ -35,9 +46,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// HTML markup used with output string, can cause issues
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body</html>\n");
+// new route handler that passes the urls.index file the URL data via res.render when /url is requested
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");        
 });
 
 // Using EXPRESS to listen port request and Printing message to to notify of listener
